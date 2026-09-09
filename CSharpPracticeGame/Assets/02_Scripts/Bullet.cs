45258b13b8e267d86fc35ca5f241d8cbb72387d1
Shaun1667 <namulletta@gmail.com>
2026-09-09 15:16:54 +0900

    feat: 플레이어 이동 및 발사 기능 구현
    WASD 플레이어 이동 구현
    Space 키 입력으로 총알 발사
    for문을 이용한 3연속 총알 생성
    Rigidbody AddForce를 이용한 총알 이동
    Invoke를 이용해 총알 2초 후 제거
    Bullet 프리팹 생성

---
+++ CSharpPracticeGame/Assets/02_Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        //Destroy(gameObject, 2);
+        Invoke("DestroySelf", 2f);
+    }
+}
