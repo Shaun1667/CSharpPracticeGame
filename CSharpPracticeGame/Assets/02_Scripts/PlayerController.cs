0c906c10d6dfa23f5b2b1b25fd0f7a54779443cb
Shaun1667 <namulletta@gmail.com>
2026-09-09 10:05:53 +0900

    init: Unity 프로젝트 초기 설정

---
+++ CSharpPracticeGame/Assets/02_Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0f, 0.1f, 0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0f, -0.1f, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.1f, 0f, 0f);
+        }
+    }
+}
